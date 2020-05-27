f6213d11fe5365f245f3a7e86f817b2c1ce64005
Stephen Kac <Stephen@StephenKac.com>
2020-05-26 21:08:15 -0600

    Reorg and added Coordinator Start

--- Sources/Application/AppDelegate.swift
+++ Sources/Application/AppDelegate.swift
@@ -11,11 +11,19 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+	var window: UIWindow?
+  private var applicationCoordinator: ApplicationCoordinator?
 
-
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
-    return true
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		
+		let window = UIWindow(frame: UIScreen.main.bounds)
+		let applicationCoordinator = ApplicationCoordinator(window: window)
+		self.window = window
+		self.applicationCoordinator = applicationCoordinator
+		
+		applicationCoordinator.start()
+		
+		return true
   }
 
   // MARK: UISceneSession Lifecycle
