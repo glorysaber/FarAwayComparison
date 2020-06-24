573e5eb786c4d1530830d1c6fb3964ea44ac5ae9
Stephen Kac <Stephen@StephenKac.com>
2020-06-23 20:56:17 -0600

    ForeignComparisonView is now setup with a new storyboard and custom views'

---
+++ Sources/ViewControllers/ForeignComparison/Views/ForeignPlanetView.swift
@@ -0,0 +1,17 @@
+//
+//  ForeignPlanetView.swift
+//  FarAwayComparison
+//
+//  Created by Stephen Kac on 6/23/20.
+//  Copyright © 2020 Stephen Kac. All rights reserved.
+//
+
+import UIKit
+
+class ForeignPlanetView: UIView {
+
+	@IBOutlet weak var diameterLabel: UILabel!
+
+	@IBOutlet weak var earthGravityUnitsLabel: UILabel!
+
+}
