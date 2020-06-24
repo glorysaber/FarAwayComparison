9b2dd56a15d25651e17482516173b7926d798fb3
Stephen Kac <Stephen@StephenKac.com>
2020-06-23 21:22:40 -0600

    Connected IBOutlets and fixed Foreign Year image

--- Sources/ViewControllers/ForeignComparison/Views/ForeignComparisonView.swift
+++ Sources/ViewControllers/ForeignComparison/Views/ForeignComparisonView.swift
@@ -13,44 +13,28 @@ class ForeignComparisonView: UIView {
 
 	@IBOutlet weak var topHorizontalStackView: UIStackView!
 
-	@IBOutlet weak var planetContainerView: UIView!
-	@IBOutlet weak var planetDiameterLine: UIImageView!
-	@IBOutlet weak var diameterLabe: UILabel!
-	@IBOutlet weak var gravityLabel: UILabel!
+	@IBOutlet weak var planetContainerView: ForeignPlanetView!
 
 	@IBOutlet weak var verticalStackStandardInfo: UIStackView!
 
 	@IBOutlet weak var comaprisonLabel: UILabel!
 
-	@IBOutlet weak var terrainStack: UIStackView!
-	@IBOutlet weak var landscapeImage: UIImageView!
-	@IBOutlet weak var landscapeLabel: UIStackView!
+	@IBOutlet weak var terrainView: ImageAndLabelView!
 
+	@IBOutlet weak var populationStack: ImageAndLabelView!
 
-	@IBOutlet weak var populationStack: UIStackView!
-	@IBOutlet weak var populationImage: UIImageView!
-	@IBOutlet weak var populationLabel: UILabel!
+	@IBOutlet weak var bottomVerticalStack: UIStackView!
 
+	@IBOutlet weak var bottomHorizontalStack: UIStackView!
 
-	@IBOutlet weak var BottomHorizontalStack: UIStackView!
+	@IBOutlet weak var earthYearEquivalenceStack: ImageAndLabelView!
 
-	@IBOutlet weak var earthEquivalenceStack: UIStackView!
+	@IBOutlet weak var earthDayEquivalenceStack: ImageAndLabelView!
 
-	@IBOutlet weak var earthYearEquivalenceStack: UIStackView!
-	@IBOutlet weak var earthYearEquivalenceImage: UIImageView!
-	@IBOutlet weak var earthYearEquivalencelabel: UILabel!
+	@IBOutlet weak var midHorizontalStack: UIStackView!
 
-	@IBOutlet weak var earthDayEquivalenceStack: UIStackView!
-	@IBOutlet weak var earthDayEquivalenceImage: UIImageView!
-	@IBOutlet weak var earthDayEquivalenceLabel: UILabel!
+	@IBOutlet weak var waterCoverageStack: ImageAndLabelView!
 
-	@IBOutlet weak var conditionsStack: UIStackView!
+	@IBOutlet weak var atmosphereStack: ImageAndLabelView!
 
-	@IBOutlet weak var waterCoverageStack: UIStackView!
-	@IBOutlet weak var waterCoverageImage: UIImageView!
-	@IBOutlet weak var waterCoveragePercentLabel: UILabel!
-
-	@IBOutlet weak var atmosphereStack: UIStackView!
-	@IBOutlet weak var atmosphereImage: UIImageView!
-	@IBOutlet weak var atmosphereLabel: UILabel!
 }
