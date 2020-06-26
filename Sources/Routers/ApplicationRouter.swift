b4cd9281f700c92cbde9ad199956516eded87f34
Stephen Kac <Stephen@StephenKac.com>
2020-06-26 16:24:54 -0600

    Added some documentation and removed unused Router protocol

--- Sources/Routers/ApplicationRouter.swift
+++ Sources/Routers/ApplicationRouter.swift
@@ -8,15 +8,21 @@
 
 import UIKit
 
-class ApplicationRouter: Routers {
+/**
+ Main Application Router used to start the chain of default scenes when launched from a normal context
+*/
+class ApplicationRouter {
 
+	/// The applications main window
   let window: UIWindow
+	/// The windows root View Controller
   weak var rootViewController: RootViewController?
   
   init(window: UIWindow) {
     self.window = window
   }
-  
+
+	/// Start the routing of the application
   func start() {
 		let rootViewController = RootViewController.instantiate()
 		rootViewController.delegate = self
@@ -29,6 +35,7 @@ class ApplicationRouter: Routers {
 }
 
 extension ApplicationRouter: RootViewControllerDelegate {
+	/// Called when the root view controller is ready to present the content of the first scene
 	func rootViewControllerDidMoveToParent() {
 		let navigationController = UINavigationController()
 		navigationController.navigationBar.isHidden = true
