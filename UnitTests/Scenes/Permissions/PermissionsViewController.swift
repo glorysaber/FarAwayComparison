3156d04fe0bbd74aeb7f019e313cd9561f4b24e4
Stephen Kac <Stephen@StephenKac.com>
2020-06-26 08:26:13 -0600

    enamed ViewCOntrollers group to Scenes

---
+++ UnitTests/Scenes/Permissions/PermissionsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PermissionsViewController.swift
+//  FarAwayComparisonTests
+//
+//  Created by Stephen Kac on 6/26/20.
+//  Copyright © 2020 Stephen Kac. All rights reserved.
+//
+
+import XCTest
+
+class PermissionsViewController: XCTestCase {
+
+	
+
+}
